aa70521af554212750a19d66aedca176c6833af9
EricQSH <qishh.13@sem.tsinghua.edu.cn>
2016-07-12 23:41:45 +0800

    Update Account.java

--- Account.java
+++ Account.java
@@ -16,19 +16,16 @@ import javax.swing.JButton;
 import com.mysql.jdbc.Connection;
 
 import java.awt.event.ActionListener;
+import java.util.Date;
 import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class Account {
 
 	JFrame frame;
-	private JTextField textVoucher;
-	private JTextField textAccount;
-	private JTextField textAmount;
-	private JTextField textSupplier;
-	private JTextField textCustomer;
-	private JTextField textItem;
-	private JTextField textDescription;
-	private JTextField textAcctDate;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -62,91 +59,14 @@ public class Account {
 	private void initialize(final DBConnect dbconn, final int UserNo) {
 		//Check if dbconn is null and UserNo is -1; if so, not connect to the database or not login yet.
 		frame = new JFrame();
-		frame.setBounds(100, 100, 500, 430);
+		frame.setBounds(100, 100, 1000, 370);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JLabel lblTitle = new JLabel("Account");
 		lblTitle.setFont(new Font("Times New Roman", Font.BOLD, 18));
-		lblTitle.setBounds(214, 21, 73, 15);
+		lblTitle.setBounds(480, 21, 73, 15);
 		frame.getContentPane().add(lblTitle);
-		
-		JLabel lblVoucherno = new JLabel("VoucherNo.");
-		lblVoucherno.setBounds(44, 71, 65, 15);
-		frame.getContentPane().add(lblVoucherno);
-		
-		textVoucher = new JTextField();
-		textVoucher.setBounds(110, 71, 120, 21);
-		frame.getContentPane().add(textVoucher);
-		textVoucher.setColumns(10);
-		
-		JLabel lblAccount = new JLabel("Account");
-		lblAccount.setBounds(44, 108, 54, 15);
-		frame.getContentPane().add(lblAccount);
-		
-		textAccount = new JTextField();
-		textAccount.setColumns(10);
-		textAccount.setBounds(110, 108, 120, 21);
-		frame.getContentPane().add(textAccount);
-		
-		JLabel lblAmount = new JLabel("Amount");
-		lblAmount.setBounds(44, 147, 54, 15);
-		frame.getContentPane().add(lblAmount);
-		
-		textAmount = new JTextField();
-		textAmount.setBounds(110, 144, 50, 21);
-		frame.getContentPane().add(textAmount);
-		textAmount.setColumns(10);
-		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Debit", "Credit"}));
-		comboBox.setBounds(165, 144, 67, 21);
-		frame.getContentPane().add(comboBox);
-		
-		JLabel lblSupplier = new JLabel("SupplierNo.");
-		lblSupplier.setBounds(272, 71, 73, 15);
-		frame.getContentPane().add(lblSupplier);
-		
-		textSupplier = new JTextField();
-		textSupplier.setBounds(355, 71, 80, 21);
-		frame.getContentPane().add(textSupplier);
-		textSupplier.setColumns(10);
-		
-		JLabel lblCustomer = new JLabel("CustomerNo.");
-		lblCustomer.setBounds(272, 108, 73, 15);
-		frame.getContentPane().add(lblCustomer);
-		
-		textCustomer = new JTextField();
-		textCustomer.setColumns(10);
-		textCustomer.setBounds(355, 108, 80, 21);
-		frame.getContentPane().add(textCustomer);
-		
-		JLabel lblItem = new JLabel("ItemNo.");
-		lblItem.setBounds(272, 147, 54, 15);
-		frame.getContentPane().add(lblItem);
-		
-		textItem = new JTextField();
-		textItem.setColumns(10);
-		textItem.setBounds(355, 144, 80, 21);
-		frame.getContentPane().add(textItem);
-		
-		JLabel lblDescription = new JLabel("Description:");
-		lblDescription.setBounds(44, 189, 80, 15);
-		frame.getContentPane().add(lblDescription);
-		
-		textDescription = new JTextField();
-		textDescription.setBounds(134, 189, 301, 21);
-		frame.getContentPane().add(textDescription);
-		textDescription.setColumns(10);
-		
-		JLabel lblAcctDate = new JLabel("AccountDate:");
-		lblAcctDate.setBounds(44, 231, 80, 15);
-		frame.getContentPane().add(lblAcctDate);
-		
-		textAcctDate = new JTextField();
-		textAcctDate.setBounds(134, 228, 96, 21);
-		frame.getContentPane().add(textAcctDate);
-		textAcctDate.setColumns(10);
 		
 		JButton btnAcctVou = new JButton("Account Voucher");
 		btnAcctVou.addActionListener(new ActionListener() {
@@ -156,38 +76,38 @@ public class Account {
 				frame.setVisible(false);
 			}
 		});
-		btnAcctVou.setBounds(44, 286, 170, 23);
+		btnAcctVou.setBounds(328, 255, 170, 23);
 		frame.getContentPane().add(btnAcctVou);
 		
 		JButton btnOriginalVou = new JButton("Original Voucher");
 		btnOriginalVou.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Voucher voucher=new Voucher(dbconn, UserNo);
+				voucher.frame.setLocation(0,350);
 				voucher.frame.setVisible(true);
-				frame.setVisible(false);
 			}
 		});
-		btnOriginalVou.setBounds(265, 286, 170, 23);
+		btnOriginalVou.setBounds(549, 255, 170, 23);
 		frame.getContentPane().add(btnOriginalVou);
 		
 		JButton btnRecord = new JButton("Record");
-		btnRecord.setBounds(44, 333, 80, 23);
+		btnRecord.setBounds(328, 289, 80, 23);
 		frame.getContentPane().add(btnRecord);
 		
 		JButton btnClear = new JButton("Clear");
 		btnClear.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				textVoucher.setText("");
+				/*textVoucher.setText("");
 				textAccount.setText("");
 				textAmount.setText("");
 				textSupplier.setText("");
 				textCustomer.setText("");
 				textItem.setText("");
 				textDescription.setText("");
-				textAcctDate.setText("");
+				textAcctDate.setText("");*/
 			}
 		});
-		btnClear.setBounds(148, 333, 80, 23);
+		btnClear.setBounds(432, 289, 80, 23);
 		frame.getContentPane().add(btnClear);
 		
 		JButton btnExit = new JButton("Exit");
@@ -197,7 +117,7 @@ public class Account {
 				System.exit(0);
 			}
 		});
-		btnExit.setBounds(355, 333, 80, 23);
+		btnExit.setBounds(639, 289, 80, 23);
 		frame.getContentPane().add(btnExit);
 		
 		JButton btnBack = new JButton("Back");
@@ -208,7 +128,47 @@ public class Account {
 				frame.setVisible(false);
 			}
 		});
-		btnBack.setBounds(252, 333, 80, 23);
+		btnBack.setBounds(536, 289, 80, 23);
 		frame.getContentPane().add(btnBack);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 50, 964, 186);
+		frame.getContentPane().add(scrollPane);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null},
+			},
+			new String[] {
+				"VoucherNo.", "Account", "SupplierNo.", "CustomerNo.", "ProductNo.", "Amount", "D/C", "Description", "Date", "Source"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				String.class, String.class, String.class, String.class, String.class, Float.class, Integer.class, String.class, Date.class, String.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		table.getColumnModel().getColumn(6).setPreferredWidth(50);
+		scrollPane.setViewportView(table);
+		
+		JLabel lblUserNo = new JLabel("");
+		lblUserNo.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		lblUserNo.setBounds(10, 22, 125, 15);
+		if(UserNo==-1)
+			lblUserNo.setText("UserNo.: not log in");
+		else lblUserNo.setText("UserNo.: "+UserNo);
+		frame.getContentPane().add(lblUserNo);
 	}
 }
