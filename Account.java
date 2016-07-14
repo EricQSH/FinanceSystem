f77f5b595796a6b646967859e3746fb675c23a6e
CCCCCChen <cobechengno4@hotmail.com>
2016-07-14 13:26:48 +0800

    add some advanced function.
    Enhancing update.

--- Account.java
+++ Account.java
@@ -1,3 +1,4 @@
+import java.awt.Component;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -5,6 +6,7 @@ import javax.swing.JLabel;
 
 import java.awt.Font;
 
+import javax.swing.DefaultCellEditor;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
@@ -25,6 +27,8 @@ import java.awt.event.ActionEvent;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import javax.swing.text.TableView.TableCell;
 
 public class Account {
 
@@ -91,11 +95,11 @@ public class Account {
 				{"", null, null, null, null, null, null, null, null, null},
 			},
 			new String[] {
-				"VoucherNo.", "Account", "SupplierNo.", "CustomerNo.", "ProductNo.", "Amount", "D/C", "Description", "Date", "Source"
+				"Account", "SupplierNo.", "CustomerNo.", "ProductNo.", "Amount", "D/C", "Description", "Date", "Source"
 			}
 		) {
 			Class[] columnTypes = new Class[] {
-				String.class, String.class, String.class, String.class, String.class, String.class, /*Float.class, Integer.class,*/String.class, String.class, Date.class, String.class
+				String.class, String.class, String.class, String.class, String.class, /*Float.class, Integer.class,*/String.class, String.class, Date.class, String.class
 			};
 			public Class getColumnClass(int columnIndex) {
 				return columnTypes[columnIndex];
@@ -104,7 +108,44 @@ public class Account {
 
 		//tableSale.setModel(model);
 		table.getColumnModel().getColumn(6).setPreferredWidth(50);
-		scrollPane.setViewportView(table);
+		
+		JComboBox box = new JComboBox();  
+        box.addItem("Debit");  
+        box.addItem("Credit");  
+        TableColumn d = table.getColumn("D/C");  
+        DefaultCellEditor dce = new DefaultCellEditor(box);   
+        d.setCellEditor(dce);
+        
+        ////////////////////////////////////////////////
+        
+
+        //修改表格的默认编辑器：
+        table.getColumnModel().getColumn(7).setCellEditor(new MyButtonEditor());
+
+         
+
+//        /*
+//        这样后就能基本达到效果了。但是还要注意，对列2来说，还需要启用可编辑功能，才行，不然仍然触发不了事件的。
+//
+//        代码片段：
+//        */
+//
+//        public boolean isCellEditable(int row, int column)  
+//        {  
+//            // 带有按钮列的功能这里必须要返回true不然按钮点击时不会触发编辑效果，也就不会触发事件。   
+//            if (column == 2)  
+//            {  
+//                return true;  
+//            }  
+//            else  
+//            {  
+//                return false;  
+//            }  
+//        } 
+        
+        ////////////////////////////////////////////////
+                 
+        scrollPane.setViewportView(table);
 		
 		JButton btnAcctVou = new JButton("Account Voucher");
 		btnAcctVou.addActionListener(new ActionListener() {
@@ -143,7 +184,7 @@ public class Account {
 					if (voucherNo.equals("")) break;
 					DecimalFormat decimalFormat=new DecimalFormat(".00");
 					double amount = 0;
-					String samount = (String) table.getValueAt(i, 5);
+					String samount = (String) table.getValueAt(i, 4);
 					boolean decimal = true;
 					for (int i1=0;i1<samount.length();i1++)
 					{
@@ -163,7 +204,7 @@ public class Account {
 							t *= 10;
 						}
 					}
-					int dc = (table.getValueAt(i, 6).equals("Debit"))?0:1;
+					int dc = (table.getValueAt(i, 5).equals("Debit"))?0:1;
 					sumc += dc==0?0:amount;
 					sumd += dc==0?amount:0;
 					
@@ -215,13 +256,13 @@ public class Account {
 						}
 						voucherNo = (String) table.getValueAt(i, 0);
 						if (voucherNo.equals("")) break;
-						String account = (String) table.getValueAt(i, 1);
-						String supplierno = (String) table.getValueAt(i, 2);
-						String customerno = (String) table.getValueAt(i, 3);
-						String productno = (String) table.getValueAt(i, 4);
+						String account = (String) table.getValueAt(i, 0);
+						String supplierno = (String) table.getValueAt(i, 1);
+						String customerno = (String) table.getValueAt(i, 2);
+						String productno = (String) table.getValueAt(i, 3);
 						DecimalFormat decimalFormat=new DecimalFormat(".00");
 						double amount = 0;
-						String samount = (String) table.getValueAt(i, 5);
+						String samount = (String) table.getValueAt(i, 4);
 						boolean decimal = true;
 						for (int i1=0;i1<samount.length();i1++)
 						{
@@ -241,12 +282,12 @@ public class Account {
 								t *= 10;
 							}
 						}
-						int dc = (table.getValueAt(i, 6).equals("Debit"))?0:1;
+						int dc = (table.getValueAt(i, 5).equals("Debit"))?0:1;
 						sumc += dc==0?0:amount;
 						sumd += dc==0?amount:0;
-						String description = (String) table.getValueAt(i, 7);
-						Date date = (Date) table.getValueAt(i, 8);
-						String source = (String) table.getValueAt(i, 9);			
+						String description = (String) table.getValueAt(i, 6);
+						Date date = (Date) table.getValueAt(i, 7);
+						String source = (String) table.getValueAt(i, 8);			
 						
 						System.out.print("INSERT INTO account values("+voucherno+","+account+","+amount+","+dc+","+date+");" + "\n");
 						if (sumd - sumc==0) 
@@ -312,4 +353,92 @@ public class Account {
 		else lblUserNo.setText("UserNo.: "+UserNo);
 		frame.getContentPane().add(lblUserNo);
 	}
+	
+	////////////////////////////////////////////////////////////
+	public class MyButtonEditor extends DefaultCellEditor  
+    {  
+      
+        /** 
+         * serialVersionUID 
+         */  
+        private static final long serialVersionUID = -6546334664166791132L;  
+      
+        private JPanel panel;  
+      
+        private JButton button;  
+      
+        public MyButtonEditor()  
+        {  
+            // DefautlCellEditor有此构造器，需要传入一个，但这个不会使用到，直接new一个即可。   
+            super(new JTextField());  
+      
+            // 设置点击几次激活编辑。   
+            this.setClickCountToStart(1);  
+      
+            this.initButton();  
+      
+            this.initPanel();  
+      
+            // 添加按钮。   
+            this.panel.add(this.button);  
+        }  
+      
+        private void initButton()  
+        {  
+            this.button = new JButton();  
+      
+            // 设置按钮的大小及位置。   
+            this.button.setBounds(0, 0, 50, 15);  
+      
+            // 为按钮添加事件。这里只能添加ActionListner事件，Mouse事件无效。   
+            this.button.addActionListener(new ActionListener()  
+            {  
+                public void actionPerformed(ActionEvent e)  
+                {  
+                    // 触发取消编辑的事件，不会调用tableModel的setValue方法。   
+                    MyButtonEditor.this.fireEditingCanceled();
+                    int row = table.getSelectedRow();
+                    int col = table.getSelectedColumn();
+                    System.out.print("Performed Success! Col: " + col + " Row: " + row + "\n");
+                    DateUI dateTable = new DateUI(table, row, col);
+                    // 这里可以做其它操作。   
+                    // 可以将table传入，通过getSelectedRow,getSelectColumn方法获取到当前选择的行和列及其它操作等。   
+                }  
+            });  
+      
+        }  
+      
+        private void initPanel()  
+        {  
+            this.panel = new JPanel();  
+      
+            // panel使用绝对定位，这样button就不会充满整个单元格。   
+            this.panel.setLayout(null);  
+        }  
+      
+      
+        /** 
+         * 这里重写父类的编辑方法，返回一个JPanel对象即可（也可以直接返回一个Button对象，但是那样会填充满整个单元格） 
+         */  
+        @Override  
+        public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column)  
+        {  
+            // 只为按钮赋值即可。也可以作其它操作。   
+            this.button.setText(value == null ? "" : String.valueOf(value));  
+      
+            return this.panel;  
+        }  
+      
+        /** 
+         * 重写编辑单元格时获取的值。如果不重写，这里可能会为按钮设置错误的值。 
+         */  
+        @Override  
+        public Object getCellEditorValue()  
+        {  
+            return this.button.getText();  
+        }  
+      
+    } 
+	
+	////////////////////////////////////////////////////////////
 }
